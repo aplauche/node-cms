a3a54d7568ff05afbb86ff51b433253f71f2deea
Anton Plauche <antonplauche92@gmail.com>
2020-09-16 20:02:27 -0500

    general post routes for review

--- models/Post.js
+++ models/Post.js
@@ -1,17 +1,33 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const slug = require('slugs')
 
 const postSchema = new Schema({
     title: {
         type: String,
         required: 'You must enter a title',
+        trim: true,
+        unique: true
 
     },
     content:{
         type: String
-    }
+    },
+    metaTitle: String,
+    metaDesc: String,
+    slug: String
 },{
     timestamps: true
+})
+
+postSchema.pre('save', function(next){
+    if(!this.isModified('title')) {
+        next()
+        return
+    }
+    // If name was modified set up the slug
+    this.slug = slug(this.title)
+    next()
 })
 
 
--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose')
 const express = require('express')
 
+
 const Posts = require('../models/Post')
 
 const postRouter = express.Router()
 
 
 postRouter.route('/')
-    .get()
-    .post((req,res,next)=>{
+    .get(async (req,res) => {
+        try {
+            const posts = await Posts.find({})
+            if(posts.length > 0){
+                res.status(200).json(posts)
+            } else {
+                res.status(200).json({status: 'No Posts Found...'})
+            }
+        } catch {
+            res.status(500).json({err: 'Server error'})
+        }
+        
+    })
+    .post((req,res)=>{
         Posts.create(req.body)
             .then(post => {
                 res.status(200).json(post)
@@ -17,8 +30,18 @@ postRouter.route('/')
                 res.status(500).json({err: err})
             })
     })
-    .put()
-    .delete()
+    .put((req,res) => {
+        res.status(403).end('PUT not supported on this endpoint')
+    })
+    .delete(async (req,res)=>{
+        try {
+            const result = await Posts.remove({})
+            res.status(200).json({status: result})
+        } catch {
+            res.status(500).json({err: 'DELETE operation failed'})
+        }
+        
+    })
 
 postRouter.route('/:postId')
     .get()
