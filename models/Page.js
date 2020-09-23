bee5a18252db42a08335366d3c1f7325823204ce
Anton Plauche <antonplauche92@gmail.com>
2020-09-23 12:36:56 -0500

    added page model

---
+++ models/Page.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+const slugs = require("slugs");
+
+const pageSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: "You must enter a title",
+      unique: true,
+    },
+    content: {
+      type: String,
+    },
+    featuredImage: {
+      type: String,
+    },
+    metaTitle: String,
+    metaDesc: String,
+    slug: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+pageSchema.pre("save", function (next) {
+  if (!this.isModified("title")) {
+    next();
+    return;
+  }
+  // If name was modified set up the slug
+  this.slug = slug(this.title);
+  next();
+});
+
+const Page = mongoose.model("Page", pageSchema);
+
+module.exports = Page;
