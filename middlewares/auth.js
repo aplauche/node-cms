b39d62ce24b508bd2b6206ffa26124b4fb8c3fbe
Anton Plauche <antonplauche92@gmail.com>
2020-09-24 18:58:25 -0500

    fixed issue #1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,21 @@
-const Users = require('../models/User')
-const jwt = require('jsonwebtoken')
+const Users = require("../models/User");
+const jwt = require("jsonwebtoken");
 
-exports.isAuthenticated = async (req,res,next) => {
-    try {
+exports.isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
 
-        const token = req.headers.authorization.split(" ")[1];
- 
-        jwt.verify(token, process.env.JWTSECRET, async (err, data) => {
+    jwt.verify(token, process.env.JWTSECRET, async (err, data) => {
+      if (err) return next(err);
+      const user = await Users.findById(data);
 
-            if (err) return next(err);
-            const user = await Users.findById(data)
+      // clear out password data before attaching to request
+      delete user.password;
 
-            // clear out password data before attaching to request
-            user.password = 0
-            
-            req.user = user;
-            next();
-            
-        })
-
-    } catch(err) {
-        next(err)
-    }
-}
-
+      req.user = user;
+      next();
+    });
+  } catch (err) {
+    next(err);
+  }
+};
