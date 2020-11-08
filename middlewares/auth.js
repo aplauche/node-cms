7fe7890285a350f00fbcec0f9d272427de8c26f3
Anton Plauche <antonplauche92@gmail.com>
2020-11-08 11:19:08 -0600

    added api key function

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,18 +3,33 @@ const jwt = require("jsonwebtoken");
 
 exports.isAuthenticated = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const apiKey = req.headers["x-api-key"];
 
-    jwt.verify(token, process.env.JWTSECRET, async (err, data) => {
-      if (err) return next(err);
-      const user = await Users.findById(data);
-
-      // clear out password data before attaching to request
-      delete user.password;
-
-      req.user = user;
+    if (apiKey == process.env.APIKEY && req.method == "GET") {
       next();
-    });
+    } else {
+      if (!req.headers.authorization) {
+        res
+          .status(401)
+          .send("You are not authorized to complete that operation");
+      }
+
+      const token = req.headers.authorization.split(" ")[1];
+
+      jwt.verify(token, process.env.JWTSECRET, async (err, data) => {
+        if (err)
+          res
+            .status(401)
+            .send("You are not authorized to complete that operation");
+        const user = await Users.findById(data);
+
+        // clear out password data before attaching to request
+        delete user.password;
+
+        req.user = user;
+        next();
+      });
+    }
   } catch (err) {
     next(err);
   }
